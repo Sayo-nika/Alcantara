0d14ea06e33c4c23d04c31707b9965984b504cbb
Capuccino <enra@sayonika.moe>
2019-01-09 01:25:59 +0800

    feat: port linux targeting
    Signed-off-by: Capuccino <enra@sayonika.moe>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,35 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart'
+  show debugDefaultTargetPlatformOverride;
 
-void main() => runApp(MyApp());
+void main()
+{
+  _setTargetPlatform();
+  runApp(MyApp());
+}
+
+// Sets the Target Platform Overrides
+// Needed for Dynamic App Behavior for Flutter 
+// See: https://github.com/google/flutter-desktop-embedding/blob/master/Flutter-Requirements.md
+void _setTargetPlatform()
+{
+  TargetPlatform targetPlatform;
+
+  if (Platform.isMacOS)
+  {
+    targetPlatform = TargetPlatform.iOS;
+  } 
+  else if (Platform.isWindows || Platform.isLinux || Platform.isFuchsia)
+  {
+    targetPlatform = TargetPlatform.android;
+  }
+
+  if (TargetPlatform != null)
+  {
+    debugDefaultTargetPlatformOverride = targetPlatform;
+  }
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
