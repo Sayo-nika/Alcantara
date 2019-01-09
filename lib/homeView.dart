32caa217d57f7dfe51dc27481ab3dc48962e6f34
Capuccino <enra@sayonika.moe>
2019-01-09 11:13:18 +0800

    feat: begin real app layout
    Signed-off-by: Capuccino <enra@sayonika.moe>

---
+++ lib/homeView.dart
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2019 (c) The Sayonika Project Authors
+ * 
+ * Licensed under GNU Affero GPL v3. You should have recieved a copy of the License with the source code.
+ * if not, god be with ye.
+ */
+
+
+import 'package:flutter/material.dart';
+
+class AlcantaraHomeView extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(255, 255, 255, 0),
+      ),
+      body: Center(
+        child: Text("Work In Progress!")
+      )
+    );
+  }
+}
---
+++ lib/settingsView.dart
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2019 (c) The Sayonika Project Authors
+ * 
+ * Licensed under GNU Affero GPL v3. You should have recieved a copy of the License with the source code.
+ * if not, god be with ye.
+ */
+
+import 'package:flutter/material.dart';
+
+class AlcantaraSettingsView extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Alcantara Settings"),
+        backgroundColor: Colors.red[400]
+      ),
+      body: Center(
+        child: Text("Work in progress!"),
+      )
+    );
+  }
+}
